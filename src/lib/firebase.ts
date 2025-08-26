02b14ad1a9e6a7c70ee55c19abfcbb812e661e65
Henrique Doebber <doebber23@gmail.com>
2025-08-26 02:56:08 +0000

    coloque a apikey do firebase no env e puxe de la

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -8,7 +9,7 @@ const firebaseConfig = {
   projectId: "nextkanban-e4xlu",
   appId: "1:703891136551:web:8d558459c2a39e5e44ee02",
   storageBucket: "nextkanban-e4xlu.firebasestorage.app",
-  apiKey: "AIzaSyA4qvxSpHrcfDuMm2egQFfcYMg10evMDdc",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "nextkanban-e4xlu.firebaseapp.com",
   messagingSenderId: "703891136551"
 };
