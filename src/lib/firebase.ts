63276cae334976cf0cbaa6ea9ebff7c682c2147e
Henrique Doebber <doebber23@gmail.com>
2025-08-26 03:59:21 +0000

    [2025-08-26T03:58:10.494Z]  @firebase/firestore: Firestore (11.9.0): Web

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,18 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+const firebaseApiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+
+if (!firebaseApiKey) {
+  throw new Error("NEXT_PUBLIC_FIREBASE_API_KEY is not set in the environment variables. Please provide it in your .env file.");
+}
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   projectId: "nextkanban-e4xlu",
   appId: "1:703891136551:web:8d558459c2a39e5e44ee02",
   storageBucket: "nextkanban-e4xlu.firebasestorage.app",
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: firebaseApiKey,
   authDomain: "nextkanban-e4xlu.firebaseapp.com",
   messagingSenderId: "703891136551"
 };
